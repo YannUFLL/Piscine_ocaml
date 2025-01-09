f04fa545f693db39a897f7d196c04a3947e0e057
YannUFLL <yannlescoul79@free.fr>
2025-01-09 12:57:42 +0000

    MOD Format ft_print_comb2

--- day0/ex09/ft_print_comb2.ml
+++ day0/ex09/ft_print_comb2.ml
@@ -6,10 +6,9 @@
 (*   By: root <root@student.42.fr>                  +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2025/01/08 17:20:21 by root              #+#    #+#             *)
-(*   Updated: 2025/01/08 19:30:58 by root             ###   ########.fr       *)
+(*   Updated: 2025/01/09 12:57:12 by root             ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
-
 
 let ft_print_comb2 ()  = 
   let rec increment_number_2 x y = 
@@ -27,7 +26,6 @@ let ft_print_comb2 ()  =
     if (x < 99) then (increment_number_2 x (x + 1); increment_number_1 (x + 1)) else () in 
   increment_number_1 0;
   print_char '\n'
-
 
 let () = 
     ft_print_comb2 ()
