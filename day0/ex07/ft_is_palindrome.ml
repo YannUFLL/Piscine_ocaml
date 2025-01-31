aca44d98b23983b93eb7fda4f869142d8ef16de7
YannUFLL <yannlescoul79@free.fr>
2025-01-31 10:40:52 +0000

    fix: ft_is_palindrome

--- day0/ex07/ft_is_palindrome.ml
+++ day0/ex07/ft_is_palindrome.ml
@@ -6,7 +6,7 @@
 (*   By: ydumaine <ydumaine@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2025/01/07 18:45:10 by ydumaine          #+#    #+#             *)
-(*   Updated: 2025/01/29 18:34:02 by ydumaine         ###   ########.fr       *)
+(*   Updated: 2025/01/31 10:34:54 by ydumaine         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -14,14 +14,13 @@ let ft_is_palindrome str =
   let length = String.length str in
   if (length = 0)
     then true
-  else if ((length mod 2 <> 1))
-    then false
   else 
   let rec ft_compare_char x last_char = 
-    if (x <= last_char / 2 &&  String.get str x = String.get str (last_char - x))
-      then (ft_compare_char (x + 1) last_char && true)
+    if (x > last_char / 2) then true
+    else if (String.get str x = String.get str (last_char - x))
+      then (ft_compare_char (x + 1) last_char)
     else 
-      true in
+      false in
   ft_compare_char 0 (length - 1)
 
  
@@ -35,4 +34,6 @@ let () =
     if (ft_is_palindrome  "") then print_endline("true")
   else print_endline("false");
     if (ft_is_palindrome  "lamasamal") then print_endline("true")
+  else print_endline("false");
+    if (ft_is_palindrome  "abba") then print_endline("true")
   else print_endline("false");
