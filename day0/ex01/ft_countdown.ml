93c369c7abee55a4ce5060dd7cd88e99dd5db0aa
YannUFLL <yannlescoul79@free.fr>
2025-01-31 10:23:07 +0000

    mod: ft_countdown handle negative number

--- day0/ex01/ft_countdown.ml
+++ day0/ex01/ft_countdown.ml
@@ -6,17 +6,25 @@
 (*   By: ydumaine <ydumaine@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2025/01/07 17:34:30 by ydumaine          #+#    #+#             *)
-(*   Updated: 2025/01/29 18:34:02 by ydumaine         ###   ########.fr       *)
+(*   Updated: 2025/01/31 10:22:47 by ydumaine         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 let rec ft_countdown x = 
-  print_int(x);
-  print_char('\n');
-  if (x > 0) then ft_countdown(x - 1)
+  if (x < 0) then 
+    begin 
+      print_int 0;
+      print_char '\n'
+    end 
+  else 
+    begin
+      print_int x;
+      print_char '\n';
+      if (x > 0) then ft_countdown(x - 1)
+    end
 
 let () = 
   ft_countdown(9);
   ft_countdown(0);
   ft_countdown(4);
-  
+  ft_countdown(-1);
