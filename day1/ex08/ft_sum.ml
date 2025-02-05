ed765c5365053ec5cd8f64a0b8e5776f1c169b41
YannUFLL <yannlescoul79@free.fr>
2025-02-05 12:05:26 +0000

    mod: ft_sum is now tail recursive

--- day1/ex08/ft_sum.ml
+++ day1/ex08/ft_sum.ml
@@ -6,14 +6,18 @@
 (*   By: ydumaine <ydumaine@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2025/02/04 22:07:09 by ydumaine          #+#    #+#             *)
-(*   Updated: 2025/02/04 22:17:11 by ydumaine         ###   ########.fr       *)
+(*   Updated: 2025/02/05 12:04:35 by ydumaine         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
-let rec ft_sum f lower upper = 
+let ft_sum f lower upper = 
   if upper < lower then nan
-  else if lower == upper then f lower
-  else f lower +. ft_sum f (lower + 1) upper
+  else 
+  let rec aux acc n = 
+  if n > upper then acc
+  else  aux (acc +. f n) (n + 1) in
+  aux 0.0 lower
+  
 
   let () =
   print_float (ft_sum (fun i -> float_of_int (i * i)) 1 10); print_newline ();
