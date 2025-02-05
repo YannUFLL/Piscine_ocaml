2ad4a8c222722be1d3e4d53a505433c67d4d37a0
YannUFLL <yannlescoul79@free.fr>
2025-02-05 11:45:05 +0000

    mod: renamed repeat_string

--- day1/ex01/repeat_string.ml
+++ day1/ex01/repeat_string.ml
@@ -1,16 +1,16 @@
 (* ************************************************************************** *)
 (*                                                                            *)
 (*                                                        :::      ::::::::   *)
-(*   repeat_str.ml                                      :+:      :+:    :+:   *)
+(*   repeat_string.ml                                   :+:      :+:    :+:   *)
 (*                                                    +:+ +:+         +:+     *)
 (*   By: ydumaine <ydumaine@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2025/01/29 18:26:23 by ydumaine          #+#    #+#             *)
-(*   Updated: 2025/01/30 14:43:54 by ydumaine         ###   ########.fr       *)
+(*   Updated: 2025/02/05 11:07:05 by ydumaine         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
-let repeat_str ?(str="x") n = 
+let repeat_string ?(str="x") n = 
   let rec loop acc x= 
     if (x < 0) then "Error"
     else if (x = 0) then acc
@@ -20,9 +20,9 @@ let repeat_str ?(str="x") n =
     loop str n
 
 let () = 
-    print_endline (repeat_str (-1));
-    print_endline (repeat_str (0));
-    print_endline (repeat_str ~str:"Hello" (-1));
-    print_endline (repeat_str ~str:"Hello" (0));
-    print_endline (repeat_str ~str:"Hello" (3));
-    print_endline (repeat_str ~str:"Hello" (10))
+    print_endline (repeat_string (-1));
+    print_endline (repeat_string (0));
+    print_endline (repeat_string ~str:"Hello" (-1));
+    print_endline (repeat_string ~str:"Hello" (0));
+    print_endline (repeat_string ~str:"Hello" (3));
+    print_endline (repeat_string ~str:"Hello" (10))
