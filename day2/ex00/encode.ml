5ea8561b187bcfb20b7d12bae205bd00f15e7954
YannUFLL <yannlescoul79@free.fr>
2025-02-07 17:51:53 +0000

    add: ex00 ex01

---
+++ day2/ex00/encode.ml
@@ -0,0 +1,31 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   encode.ml                                          :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: ydumaine <ydumaine@student.42.fr>          +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2025/02/07 12:33:16 by ydumaine          #+#    #+#             *)
+(*   Updated: 2025/02/07 15:51:57 by ydumaine         ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+let encode l = 
+let rec aux count acc  = function 
+    | [] -> acc
+    | head :: [] -> (count, head) :: acc
+    | head :: next :: tail -> if (head = next ) then aux (count + 1) acc (next :: tail)
+                              else (count, head) :: aux 1 acc (next :: tail) in
+    aux 1 [] l
+
+let rec print_list = function 
+  | [] ->  print_string "" 
+  | (a, e) :: list -> print_string  "("; print_int a; print_string  ",\'a)"; print_list list
+
+
+let () =  
+  print_list (encode []); print_newline ();
+  print_list (encode ["lol";"lol";"Hello";"lol";"lol";"Hi";"lol"]); print_newline ();
+  print_list (encode [1; 1; 1]); print_newline ();
+  print_list (encode [1.5; 1.5; 1.5; 1.5; 1.5; 1.5; 1.5; 1.5; 1.5; 1.5; 1.5; 1.5; 1.5; 1.5; 1.5; 18.0; 18.0]); print_newline ()
+     
---
+++ day2/ex01/crossover.ml
@@ -0,0 +1,50 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   crossover.ml                                       :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: ydumaine <ydumaine@student.42.fr>          +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2025/02/07 15:56:58 by ydumaine          #+#    #+#             *)
+(*   Updated: 2025/02/07 17:46:43 by ydumaine         ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+let crossover l1 l2 = 
+  let rec is_in_list value list = match list with
+    | [] -> false
+    | head :: tail ->  if (value = head) then true else is_in_list value tail
+in 
+  let rec create_new_list acc a = 
+    match a with
+    | [] -> acc
+    | head :: tail  ->  if not (is_in_list head acc) && is_in_list head l2 then create_new_list (head :: acc) tail  
+    else create_new_list acc tail in
+  create_new_list [] l1
+
+
+  let () =
+    let test_crossover l1 l2 expected =
+    let result = crossover l1 l2 in 
+    if (result = expected ) then
+    Printf.printf "Test passed for (%s) (%s)\n" 
+      (String.concat ";" (List.map string_of_int l1))  
+      (String.concat ";" (List.map string_of_int l2))
+    else 
+    Printf.printf "Test failed for (%s) (%s), got (%s)\n" 
+      (String.concat ";" (List.map string_of_int l1))  
+      (String.concat ";" (List.map string_of_int l2))
+      (String.concat ";" (List.map string_of_int result))
+    in 
+
+
+  test_crossover [1; 2; 3] [3; 4; 5] [3];
+  test_crossover [1; 2; 3; 4] [2; 4; 6; 8] [4; 2];
+  test_crossover [1; 2; 3] [4; 5; 6] [];
+  test_crossover [1; 1; 2; 2] [1; 2; 3; 4] [2; 1];
+  test_crossover [] [1; 2; 3] [];
+  test_crossover [] [] [];
+  test_crossover [1; 2; 3] [] [];
+  test_crossover [1; 2; 3] [1; 2; 3] [3; 2; 1];
+  test_crossover [1; 2; 3; 4; 5] [5; 4; 3; 2; 1] [5; 4; 3; 2; 1];
+  test_crossover [1; 2; 3; 4; 5] [6; 7; 8; 9; 10] [];
