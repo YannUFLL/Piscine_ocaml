1e8257c0b803abc8d81ed8b4c2de5de797a9656e
YannUFLL <yannlescoul79@free.fr>
2025-03-13 16:05:44 +0000

    add: tests

--- day4/ex00/micronap.ml
+++ day4/ex00/micronap.ml
@@ -1,17 +1,18 @@
 (* ************************************************************************** *)
 (*                                                                            *)
 (*                                                        :::      ::::::::   *)
-(*   mincronap.ml                                       :+:      :+:    :+:   *)
+(*   micronap.ml                                        :+:      :+:    :+:   *)
 (*                                                    +:+ +:+         +:+     *)
 (*   By: ydumaine <ydumaine@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2025/03/06 15:57:15 by ydumaine          #+#    #+#             *)
-(*   Updated: 2025/03/06 16:22:29 by ydumaine         ###   ########.fr       *)
+(*   Updated: 2025/03/13 14:04:39 by ydumaine         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
+let my_sleep () = Unix.sleep 1
+
 let () = 
-let my_sleep () = Unix.sleep 1 in
 try 
   let max = int_of_string Sys.argv.(1) in
   let i = ref 0 in
--- day4/ex01/ft_ref.ml
+++ day4/ex01/ft_ref.ml
@@ -6,7 +6,7 @@
 (*   By: ydumaine <ydumaine@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2025/03/06 16:24:49 by ydumaine          #+#    #+#             *)
-(*   Updated: 2025/03/07 15:48:09 by ydumaine         ###   ########.fr       *)
+(*   Updated: 2025/03/13 15:39:43 by ydumaine         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -19,3 +19,37 @@ let get x = x.contents
 let set x y = x.contents <- y
 
 let bind (x : 'a ft_ref) (f : 'a -> 'a ft_ref) : 'a ft_ref = f x.contents
+
+let print_result expected result =
+  Printf.printf "Result: %s | Expected: %s %s\n"
+    result expected (if result = expected then "✅" else "❌")
+
+let () =
+  let r = return 42 in
+  print_result "42" (string_of_int (get r));
+
+  set r 84;
+  print_result "84" (string_of_int (get r));
+
+  set r (-999);
+  print_result "-999" (string_of_int (get r));
+
+  let str_ref = return "Hello" in
+  print_result "Hello" (get str_ref);
+
+  set str_ref "OCaml";
+  print_result "OCaml" (get str_ref);
+
+  let double x = return (x * 2) in
+  let r2 = bind r double in
+  print_result "-1998" (string_of_int (get r2));
+
+  let greet name = return ("Hello, " ^ name) in
+  let r3 = bind str_ref greet in
+  print_result "Hello, OCaml" (get r3);
+
+  let float_ref = return 3.14 in
+  print_result "3.14" (string_of_float (get float_ref));
+
+  set float_ref 2.718;
+  print_result "2.718" (string_of_float (get float_ref));
