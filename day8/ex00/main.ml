5846d5cac3bfc2a15141a615aae428430621ca71
YannUFLL <yannlescoul79@free.fr>
2025-06-04 13:13:33 +0000

    mod: main.ml

--- day8/ex00/main.ml
+++ day8/ex00/main.ml
@@ -6,7 +6,7 @@
 (*   By: ydumaine <ydumaine@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2025/05/15 18:48:06 by ydumaine          #+#    #+#             *)
-(*   Updated: 2025/05/16 12:36:33 by ydumaine         ###   ########.fr       *)
+(*   Updated: 2025/06/04 13:12:45 by ydumaine         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -17,8 +17,11 @@ let () =
   let added = Watchtower.Watchtower.add h1 h2 in
   let subbed = Watchtower.Watchtower.sub h1 h2 in
 
-  Printf.printf "%d + %d mod 12 = %d\n" h1 h2 added;
-  Printf.printf "%d - %d mod 12 = %d\n" h1 h2 subbed;
+  Printf.printf "%d %d added: %d\n" h1 h2 added;
+  Printf.printf "%d %d subbed 12: %d\n" h1 h2 subbed;
 
-  Printf.printf "add zero %d = %d\n" h1 (Watchtower.Watchtower.add h1 Watchtower.Watchtower.zero);
-  Printf.printf "add %d zero = %d\n" h1 (Watchtower.Watchtower.add Watchtower.Watchtower.zero h1)
+  Printf.printf "zero: %d\n" Watchtower.Watchtower.zero;
+  Printf.printf "add %d with zero: %d\n" h1
+    (Watchtower.Watchtower.add h1 Watchtower.Watchtower.zero);
+  Printf.printf "add %d %d: %d\n" 12 12
+    (Watchtower.Watchtower.add 12 12)
